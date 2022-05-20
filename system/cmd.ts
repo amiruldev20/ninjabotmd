1bbdfd6bd7c8358c3a94d8f57baf8c9f9bd65552
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2022-05-20 06:09:54 +0000

    Update cmd.ts

--- system/cmd.ts
+++ system/cmd.ts
@@ -58,7 +58,8 @@ export default class CommandHandler {
           ...ev,
         });
     } catch (e) {
-      throw util.logger.format(e);
+    //  throw util.logger.format(e);
+      return console.log("tes")
     }
   };
 
